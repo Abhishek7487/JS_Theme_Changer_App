ea1799e2f054450188f7fa9070e7126ef42e4624
Abhishek7487 <dellabhishek7487@gmail.com>
2022-11-24 11:02:10 +0530

    Updated Project using React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  let [currentTheme, setCurrentTheme] = useState("light");
+
+  // Function to set Dark Theme
+  const setDarkTheme = () => {
+    document.documentElement.style.setProperty("--bg-color", "#2b2c34");
+    document.documentElement.style.setProperty("--text-color", "#fffffe");
+    document.querySelector(".button").textContent = "Try Light Mode";
+  };
+
+  // Function to set Light Theme
+  const setLightTheme = () => {
+    document.documentElement.style.setProperty("--bg-color", "#fffffe");
+    document.documentElement.style.setProperty("--text-color", "#2b2c34");
+    document.querySelector(".button").textContent = "Try Dark Mode";
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">Theme Changer App</h1>
+      <p className="description">
+        It’s basically a theme changer app which changes the theme on click of
+        the button.
+      </p>
+      <button
+        className="button"
+        onClick={() => {
+          if (currentTheme === "light") {
+            setDarkTheme();
+            setCurrentTheme("dark");
+          } else if (currentTheme === "dark") {
+            setLightTheme();
+            setCurrentTheme("light");
+          }
+        }}
+      >
+        Try dark mode!
+      </button>
+      <p className="instagramId">@abhishekpatil7770</p>
     </div>
   );
 }
-
-export default App;
